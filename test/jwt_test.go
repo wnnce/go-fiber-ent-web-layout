5f600e79350e5163d78508ce1f9c793dfbd9d9d6
crushcola <ranchangxin@gmail.com>
2024-04-16 14:21:31 +0800

    update jwt_test.go

--- test/jwt_test.go
+++ test/jwt_test.go
@@ -5,14 +5,10 @@ import (
 	"github.com/bytedance/sonic"
 	"go-fiber-ent-web-layout/internal/conf"
 	"go-fiber-ent-web-layout/internal/tools"
+	"go-fiber-ent-web-layout/internal/usercase"
 	"testing"
 	"time"
 )
-
-type user struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
 
 func TestTokenGenerate(t *testing.T) {
 	jwtConf := &conf.Jwt{
@@ -22,10 +18,11 @@ func TestTokenGenerate(t *testing.T) {
 	}
 	jwtService := tools.NewJwtService(jwtConf)
 
-	token, err := jwtService.CreateToken(&user{
-		ID:   1,
-		Name: "xin",
-	}, []string{"all", "user"})
+	token, err := jwtService.CreateToken(&usercase.User{
+		UserId:   1,
+		Username: "admin",
+		Scopes:   []string{"select", "create"},
+	})
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	} else {
@@ -39,12 +36,13 @@ func TestTokenGenerate(t *testing.T) {
 		at, _ := claims.GetExpirationTime()
 		println(at.UnixMilli())
 		sub, _ := claims.GetSubject()
-		tokenUser := &user{}
+		tokenUser := &usercase.User{}
 		err := sonic.Unmarshal([]byte(sub), tokenUser)
 		if err == nil {
 			fmt.Printf("%v\n", tokenUser)
 		}
-		scope, _ := claims.GetScope()
-		println(scope)
+		for _, v := range tokenUser.GetPermissions() {
+			fmt.Println(v)
+		}
 	}
 }
